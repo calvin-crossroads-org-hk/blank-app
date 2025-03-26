51715c868f7ea869c04386c3301506cdedc2d4a4
calvin-crossroads-org-hk <calvin.crossroads.org@gmail.com>
2025-03-26 10:49:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 My new abc app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://www.google.com)."
 )
+
+if st.button("Go !")
+    st.balloons()
+
+
